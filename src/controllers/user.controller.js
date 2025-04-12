1346f0891482504e36fa3909730fa023504b0505
Hardik <hardik.pandey.716@gmail.com>
2025-04-12 21:16:47 +0530

    user register and login backend

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,22 @@ import { User } from "../models/User.models.js";
 import { uploadOnCloudinary } from "../utils/cloudnary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+
+const generateAccessAndRefreshToken = async(userId) => {
+    try{
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken();
+        const refreshToken = user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({validateBeforeSave: false});
+
+        return {accessToken,refreshToken};
+    }catch(error){
+        throw new ApiError(500,"Something went wrong while generating refresh and access Token");
+    }
+}
+
 const registerUser = asyncHandler(async(req,res) => {
     // get user details from frontend
     const {fullname,username,email,password} = req.body;
@@ -24,7 +40,7 @@ const registerUser = asyncHandler(async(req,res) => {
 
     // check if user already exist with email or username
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{username},{email}]
     })
     if(existedUser){
@@ -32,11 +48,19 @@ const registerUser = asyncHandler(async(req,res) => {
     }
     // check for images,check for avatar
     // multer gives access for files to us
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let avatarLocalPath = ""; 
+    avatarLocalPath = req.files?.avatar[0]?.path;
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
+
+    console.log("Files: ", avatarLocalPath);
+    console.log("Files: ", coverImageLocalPath);
 
     if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar File is Required");
+        throw new ApiError(400,"AvatarLocalPath File is Required");
     }
 
     // upload them on cloudinary
@@ -48,6 +72,7 @@ const registerUser = asyncHandler(async(req,res) => {
         throw new ApiError(400,"Avatar File is Required");
     }
     // then save the user object in db if not already there - create entry in db
+    console.log("avatar: ",avatar);
 
     const user = await User.create({
         fullname,
@@ -58,22 +83,53 @@ const registerUser = asyncHandler(async(req,res) => {
         password
     })
     
+    // remove pass and refresh token field from response
+    
     const createdUser = await User.findById(user._id).select(
         "-password -refreshToken"
     );
 
+    // if not then send the error message to the user trying to create the user
+
     if(!createdUser){
         throw new ApiError(500,"Something went wrong while registering the User");
     }
-    
-
-    // remove pass and refresh token field from response
-
-    // check for user creation
 
     // if created then send the response
 
-    // if not then send the error message to the user trying to create the user
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User created Successfully!")
+    )
+    
+})
+
+const LoginUser = asyncHandler(async (req,res)=> {
+    // we will need the username and password to login
+    const {username,password} = req.body;
+
+    if([username,password].some((item) => item.trim === "")){
+        throw new ApiError(400,"write username and password");
+    }
+
+    const UserExist = User.findOne({
+        $or: [{username,email}]
+    });
+    if(!UserExist){
+        throw new ApiError(400,"No User Found with this Username");
+    }
+    const ValidUser = UserExist.isPasswordCorrect(password);
+
+    if(!ValidUser){
+        throw new ApiError(401,"User Password Incorrect");
+    }
+
+    const {accessToken,refreshToken} = await generateAccessAndRefreshToken(existedUser._id);
+
+    // now we will send this refresh and access Token to the cookies
+
+    const logedInUser = await User.findById(existedUser._id).select("-password -refreshToken");
+    
+
 })
 
 export {registerUser};
--- src/utils/cloudnary.js
+++ src/utils/cloudnary.js
@@ -3,7 +3,7 @@ import {v2 as cloudinary} from "cloudinary";
 import fs from "fs"; // it is a file system available in node helps to read write remove or update
 
 cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_AME, 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY, 
     api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
 });
@@ -14,15 +14,17 @@ const uploadOnCloudinary = async (localFilePath) => {
     try{
         if(!localFilePath) return null
         // upload the file on the cloudinary site 
-        const response = await cloudinary.uploader.upload(localPath, {
+        const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
         })
         // file has been uploaded if it reaches here
-        console.log("The file was uploaded on the cloudinary server",response.url);
+        // console.log("The file was uploaded on the cloudinary server",response.url);
+        fs.unlinkSync(localFilePath);
+        // when the file is uploaded then it is removed automatically from the folder
         return response;
     }catch(error){
         // if the file is not uploaded to cloudinary then we have to clean it from our system
-        fs.unlinkSync(localPath) // remove the locally saved temporary file as upload
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as upload
         return null;
     }
 }
