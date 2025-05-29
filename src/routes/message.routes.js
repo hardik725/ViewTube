1e593571ab30b36677b65369494a59fc9d749ab2
Hardik <hardik.pandey.716@gmail.com>
2025-05-30 01:05:18 +0530

    helper

--- src/routes/message.routes.js
+++ src/routes/message.routes.js
@@ -1,16 +1,11 @@
-import express from "express";
+import { Router } from "express";
 import { getMessage, sendMessage } from "../controllers/message.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
-const router = express.Router();
-
-// Route to send message with socket.io integration
-router.post("/sendMessage", (req, res, next) => {
-  // Attach io instance to request object
-  req.io = req.app.get("io");
-  sendMessage(req, res, next);
-});
+const router = Router();
 
 router.route("/getMessages/:channelId").post(verifyJWT,getMessage);
+
+router.route("/sendMessage").post(verifyJWT,sendMessage);
 
 export default router;
